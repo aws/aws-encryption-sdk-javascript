bd83211f0fa15c2c065563a6879c23e711bdc312
seebees <ryanemer@amazon.com>
2019-04-04 16:07:31 -0700

    feat: HKDF initial commit (#4)
    Node.js does not implement HKDF.

---
+++ modules/hkdf-node/README.md
@@ -0,0 +1,36 @@
+# @aws-crypto/hkdf-node
+
+Another HMAC-based Key Derivation Function for node.js.
+The function is very simple, but having a controlled, reviewed, and blessed version
+is valuable.  As a crypto primitive this helpful to have, but the nodejs `crypto`
+module only exposes what openssl supports.  Since it is so simple and derivable,
+there are no plans to include this in core.
+
+spec: https://tools.ietf.org/html/rfc5869
+
+## install
+
+```sh
+npm install @aws-crypto/hkdf-node
+```
+
+## use
+
+```javascript
+  const HKDF = require('@aws-crypto/hkdf-node')
+  const expand = HKDF('sha256')('some key', 'some salt')
+  const info = {some: 'info', message_id: 123}
+  const key = expand(32, Buffer.from(JSON.stringify(info)))
+```
+
+## test
+
+```sh
+npm test
+```
+
+## license
+
+This SDK is distributed under the
+[Apache License, Version 2.0](http://www.apache.org/licenses/LICENSE-2.0),
+see LICENSE.txt and NOTICE.txt for more information.
---
+++ modules/hkdf-node/src/hkdf.ts
@@ -0,0 +1,113 @@
+/*
+ * Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"). You may not use
+ * this file except in compliance with the License. A copy of the License is
+ * located at
+ *
+ *     http://aws.amazon.com/apache2.0/
+ *
+ * or in the "license" file accompanying this file. This file is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { createHmac, createHash } from 'crypto'
+import { UnsupportedAlgorithm, KeyLengthError } from './errors'
+
+/**
+ * Factory function to curry the hash algorithm
+ *
+ * @param algorithm [String] The name of the hash algorithm to use
+ * @return [Function] The extract function decorated with expand and verify functions
+ */
+export function HKDF (algorithm: string = 'sha256'): HKDFOutput {
+  // Check the length and support
+  try {
+    var hashLength = createHash(algorithm).digest().length
+  } catch (ex) {
+    throw new UnsupportedAlgorithm(algorithm)
+  }
+
+  // (<= 255*HashLen) from https://tools.ietf.org/html/rfc5869
+  const maxLength = 255 * hashLength
+
+  // decorate the return function
+  extractExpand.extract = extract
+  extractExpand.expand = expand
+
+  return extractExpand
+
+  // implementation
+
+  /**
+   * Extracts a prk and returns a function to expand the given initial key
+   *
+   * @param ikm [String|Buffer] The initial key
+   * @param salt [String|Buffer] Optional salt for the extraction
+   * @return [Function] expand function with the extracted key curried onto it
+   */
+  function extractExpand (ikm: string|Buffer, salt?: string|Buffer|false) {
+    const prk = extract(ikm, salt)
+    return (length:number, info?:Buffer) => expand(prk, length, info)
+  }
+
+  /**
+   * Extracts a prk and returns a function to expand the given initial key
+   *
+   * @param ikm [String|Buffer] The initial key
+   * @param salt [String|Buffer] Optional salt for the extraction
+   * @return [Buffer] the expanded key
+   */
+  function extract (ikm: string|Buffer, salt?: string|Buffer|false) {
+    var _salt = salt || Buffer.alloc(hashLength, 0).toString()
+    return createHmac(algorithm, _salt).update(ikm).digest()
+  }
+
+  /**
+   * Expands a given key
+   *
+   * @param prk [Buffer] The key to expand from
+   * @param length [Number] The length of the expanded key
+   * @param info [Buffer] Data to bind the expanded key to application/context specific information
+   * @return [Buffer] the expanded
+   */
+  function expand (prk:Buffer, length:number, info?:Buffer) {
+    if (length > maxLength) {
+      throw new KeyLengthError(maxLength, algorithm)
+    }
+
+    info = info || Buffer.alloc(0)
+    var N = Math.ceil(length / hashLength)
+    var memo: Buffer[] = []
+
+    /* L/length octets are returned from T(1)...T(N), and T(0) is definitionally empty/zero length.
+     * Elide T(0) into the Buffer.alloc(0) case and then return L octets of T indexed 0...L-1.
+     */
+    for (var i = 0; i < N; i++) {
+      memo[i] = createHmac(algorithm, prk)
+        .update((memo[i - 1] || Buffer.alloc(0)))
+        .update(info)
+        .update(Buffer.alloc(1, i + 1))
+        .digest()
+    }
+    return Buffer.concat(memo, length)
+  }
+}
+
+export interface Extract {
+  (ikm: string|Buffer, salt?: string|Buffer|false): Buffer
+}
+
+export interface Expand {
+  (prk:Buffer, length:number, info?:Buffer): Buffer
+}
+
+export interface HKDFOutput {
+  (...args: Parameters<Extract>): (...args: Curry<Parameters<Expand>>) => Buffer
+  extract: Extract
+  expand: Expand
+}
+
+type Curry<T extends any[]> = ((...args: T) => void) extends (head: any, ...tail: infer U) => any ? U : never
