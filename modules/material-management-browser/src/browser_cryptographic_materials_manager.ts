9b5bfa87aec57c65c13b45cb9ed44c3dfd4ac0c5
seebees <ryanemer@amazon.com>
2019-04-08 14:40:27 -0700

    feat: material-management-browser initial commit (#10)
    Material management browser

---
+++ modules/material-management-browser/src/browser_cryptographic_materials_manager.ts
@@ -0,0 +1,119 @@
+/*
+ * Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"). You may not use
+ * this file except in compliance with the License. A copy of the License is
+ * located at
+ *
+ *     http://aws.amazon.com/apache2.0/
+ *
+ * or in the "license" file accompanying this file. This file is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {
+  WebCryptoMaterialsManager, EncryptionRequest, // eslint-disable-line no-unused-vars
+  DecryptionRequest, EncryptionContext, // eslint-disable-line no-unused-vars
+  EncryptionResponse, DecryptionResponse, Keyring, // eslint-disable-line no-unused-vars
+  WebCryptoAlgorithmSuite, WebCryptoEncryptionMaterial,
+  WebCryptoDecryptionMaterial, SignatureKey, needs,
+  VerificationKey, AlgorithmSuiteIdentifier, immutableBaseClass
+} from '@aws-crypto/material-management'
+
+import { ENCODED_SIGNER_KEY } from '@aws-crypto/serialize'
+import { getWebCryptoBackend, getNonZeroByteBackend } from '@aws-crypto/web-crypto-backend'
+import { fromBase64, toBase64 } from '@aws-sdk/util-base64-browser'
+
+type WebCryptoKeyring = Keyring<WebCryptoAlgorithmSuite>
+export type WebCryptoEncryptionRequest = EncryptionRequest<WebCryptoAlgorithmSuite>
+export type WebCryptoDecryptionRequest = DecryptionRequest<WebCryptoAlgorithmSuite>
+export type WebCryptoEncryptionResponse = EncryptionResponse<WebCryptoAlgorithmSuite>
+export type WebCryptoDecryptionResponse = DecryptionResponse<WebCryptoAlgorithmSuite>
+
+export class WebCryptoCryptographicMaterialsManager implements WebCryptoMaterialsManager {
+  readonly keyring: WebCryptoKeyring
+  constructor (keyring: WebCryptoKeyring) {
+    needs(keyring instanceof Keyring, 'Unsupported type.')
+    this.keyring = keyring
+  }
+  async getEncryptionMaterials ({ suite, encryptionContext }: WebCryptoEncryptionRequest): Promise<WebCryptoEncryptionResponse> {
+    suite = suite || new WebCryptoAlgorithmSuite(AlgorithmSuiteIdentifier.ALG_AES256_GCM_IV12_TAG16_HKDF_SHA384_ECDSA_P384)
+    const material = new WebCryptoEncryptionMaterial(suite)
+
+    const context = await this._generateSigningKeyAndUpdateEncryptionContext(material, encryptionContext)
+
+    await this.keyring.onEncrypt(material, context)
+
+    /* Postcondition: The WebCryptoEncryptionMaterial must contain a valid dataKey. */
+    needs(material.hasValidKey(), 'Unencrypted data key is invalid.')
+
+    /* Postcondition: The WebCryptoEncryptionMaterial must contain at least 1 EncryptedDataKey. */
+    needs(material.encryptedDataKeys.length, 'No EncryptedDataKeys: the ciphertext can never be decrypted.')
+
+    return { material, context }
+  }
+
+  async decryptMaterials ({ suite, encryptedDataKeys, encryptionContext }: WebCryptoDecryptionRequest): Promise<WebCryptoDecryptionResponse> {
+    const material = await this._loadVerificationKeyFromEncryptionContext(new WebCryptoDecryptionMaterial(suite), encryptionContext)
+
+    await this.keyring.onDecrypt(material, encryptedDataKeys.slice(), encryptionContext)
+
+    /* Postcondition: The WebCryptoDecryptionMaterial must contain a valid dataKey. */
+    needs(material.hasValidKey(), 'Unencrypted data key is invalid.')
+
+    return { material, context: encryptionContext || {} }
+  }
+
+  async _generateSigningKeyAndUpdateEncryptionContext (material: WebCryptoEncryptionMaterial, context?: EncryptionContext) {
+    const { signatureCurve: namedCurve } = material.suite
+
+    /* Precondition: The algorithm suite specification must support a signatureCurve to generate a signing key. */
+    if (!namedCurve) return { ...context }
+
+    const backend = await getWebCryptoBackend()
+    const subtle = getNonZeroByteBackend(backend)
+
+    const webCryptoAlgorithm = { name: 'ECDSA', namedCurve }
+    const extractable = false
+    const usages = ['sign']
+    const format = 'raw'
+
+    const { publicKey, privateKey } = await subtle.generateKey(webCryptoAlgorithm, extractable, usages)
+    const publicKeyBytes = await subtle.exportKey(format, publicKey)
+    const compressPoint = SignatureKey.encodeCompressPoint(new Uint8Array(publicKeyBytes), material.suite)
+    const signatureKey = new SignatureKey(privateKey, compressPoint, material.suite)
+    material.setSignatureKey(signatureKey)
+    return { ...context, [ENCODED_SIGNER_KEY]: toBase64(compressPoint) }
+  }
+
+  async _loadVerificationKeyFromEncryptionContext (material: WebCryptoDecryptionMaterial, context?: EncryptionContext) {
+    const { signatureCurve: namedCurve } = material.suite
+
+    /* Precondition: The algorithm suite specification must support a signatureCurve to extract a verification key. */
+    if (!namedCurve) return material
+
+    /* Precondition: WebCryptoCryptographicMaterialsManager If the algorithm suite specification requires a signatureCurve a context must exist. */
+    if (!context) throw new Error('Context does not contain required public key.')
+
+    const { [ENCODED_SIGNER_KEY]: compressPoint } = context
+
+    /* Precondition: WebCryptoCryptographicMaterialsManager The context must contain the public key. */
+    needs(compressPoint, 'Context does not contain required public key.')
+
+    const backend = await getWebCryptoBackend()
+    const subtle = getNonZeroByteBackend(backend)
+    const webCryptoAlgorithm = { name: 'ECDSA', namedCurve }
+    const extractable = false
+    const usages = ['sign']
+    const format = 'raw'
+
+    const publicKeyBytes = VerificationKey.decodeCompressPoint(fromBase64(compressPoint), material.suite)
+    const publicKey = await subtle.importKey(format, publicKeyBytes, webCryptoAlgorithm, extractable, usages)
+
+    return material.setVerificationKey(new VerificationKey(publicKey, material.suite))
+  }
+}
+
+immutableBaseClass(WebCryptoCryptographicMaterialsManager)
---
+++ modules/material-management-browser/test/material_helpers.test.ts
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"). You may not use
+ * this file except in compliance with the License. A copy of the License is
+ * located at
+ *
+ *     http://aws.amazon.com/apache2.0/
+ *
+ * or in the "license" file accompanying this file. This file is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/* eslint-env mocha */
+
+// import { expect } from 'chai'
+// import 'mocha'
+// import { NodeDecryptionMaterial, NodeEncryptionMaterial, NodeAlgorithmSuite, AlgorithmSuiteIdentifier, KeyringTraceFlag } from '@aws-crypto/material-management'
+// import { getEncryptHelper, getDecryptionHelper } from '../src/material_helpers'
+// // @ts-ignore
+// import { Decipheriv, Cipheriv } from 'crypto'
+
+// describe('getEncryptHelper', () => {
+//   it('first test', () => {
+//     const suite = new NodeAlgorithmSuite(AlgorithmSuiteIdentifier.ALG_AES128_GCM_IV12_TAG16)
+//     const material = new NodeEncryptionMaterial(suite)
+//     const dataKey = new Uint8Array(suite.keyLengthBytes).fill(1)
+//     const trace = { keyNamespace: 'k', keyName: 'k', flags: KeyringTraceFlag.WRAPPING_KEY_GENERATED_DATA_KEY }
+//     material.setUnencryptedDataKey(dataKey, trace)
+
+//     const helper = getEncryptHelper(material)
+//     const getCipher = helper.kdfGetCipher()
+//     const iv = new Uint8Array(12)
+//     const cipher = getCipher(iv)
+//     expect(cipher).to.be.instanceOf(Cipheriv)
+//   })
+// })
+
+// describe('getDecryptionHelper', () => {
+//   it('first test', () => {
+//     const suite = new NodeAlgorithmSuite(AlgorithmSuiteIdentifier.ALG_AES128_GCM_IV12_TAG16)
+//     const material = new NodeDecryptionMaterial(suite)
+//     const dataKey = new Uint8Array(suite.keyLengthBytes).fill(1)
+//     const trace = { keyNamespace: 'k', keyName: 'k', flags: KeyringTraceFlag.WRAPPING_KEY_DECRYPTED_DATA_KEY }
+//     material.setUnencryptedDataKey(dataKey, trace)
+
+//     const helper = getDecryptionHelper(material)
+//     const getDecipher = helper.kdfGetDecipher()
+//     const iv = new Uint8Array(12)
+//     const decipher = getDecipher(iv)
+
+//     expect(decipher).to.be.instanceOf(Decipheriv)
+//   })
+// })
