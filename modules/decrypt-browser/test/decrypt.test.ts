aacb5b369fc8b979bd2617a0df372f51902171e1
seebees <ryanemer@amazon.com>
2019-06-20 11:18:00 -0700

    feat: browser testing code coverage (#111)
    This test insures that the entire module is included in code coverage
    
    * Karma config
    * package.json scripts
    * at least one test including the whole module

---
+++ modules/decrypt-browser/test/decrypt.test.ts
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"). You may not use
+ * this file except in compliance with the License. A copy of the License is
+ * located at
+ *
+ *     http://aws.amazon.com/apache2.0/
+ *
+ * or in the "license" file accompanying this file. This file is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/* eslint-env mocha */
+
+import { expect } from 'chai'
+import 'mocha'
+import { decrypt } from '../src/index'
+
+describe('decrypt', () => {
+  /* This test is mostly to insure that I include all files.
+   * So I can not lie on code coverage.
+   */
+  it('is a function', () => {
+    expect(decrypt).to.be.a('function')
+  })
+})
