107566ed326b1ae235191d0510b11f10f5656ab2
Ryan Emery <ryanemer@amazon.com>
2019-09-13 15:45:41 -0700

    fix: KeyringTraceFlag requirements and data key caching
    cloneMaterial incorectly required WRAPPING_KEY_GENERATED_DATA_KEY.
    in the case of DecryptionMaterials, this should never be the case.
    When I wrote the tests, I did not take this case into consideration
    and simply set the generate and decrypt flags on the same keyringTrace.
    
    The organization of the traces has also changed.
    There is now a single trace for each “operation”.
    This mean that a trace will exists for the set operation
    as well as the encrypted data key.
    This lets me require only valid trace flags for each operation.
    I simplified the cloneMaterial function to rely on the set methods
    `setUnencryptedDataKey` and `addEncryptedDataKey`
    to deny invalid fields.
    I also, organize the KeyringTraceFlags to determin which flags are valid.
    
    The KMS Keyring needed to be updated
    to make sure that the trace contains the WRAPPING_KEY_SIGNED_ENC_CTX
    flag.
    
    Tests are updated, and added.

--- modules/material-management/test/clone_cryptographic_material.test.ts
+++ modules/material-management/test/clone_cryptographic_material.test.ts
@@ -34,10 +34,15 @@ import {
 const nodeSuite = new NodeAlgorithmSuite(AlgorithmSuiteIdentifier.ALG_AES128_GCM_IV12_TAG16)
 const webCryptoSuite = new WebCryptoAlgorithmSuite(AlgorithmSuiteIdentifier.ALG_AES128_GCM_IV12_TAG16)
 const udk128 = new Uint8Array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16])
-const trace = {
+const encryptTrace = {
   keyNamespace: 'keyNamespace',
   keyName: 'keyName',
-  flags: KeyringTraceFlag.WRAPPING_KEY_GENERATED_DATA_KEY | KeyringTraceFlag.WRAPPING_KEY_DECRYPTED_DATA_KEY
+  flags: KeyringTraceFlag.WRAPPING_KEY_GENERATED_DATA_KEY
+}
+const decryptTrace = {
+  keyNamespace: 'keyNamespace',
+  keyName: 'keyName',
+  flags: KeyringTraceFlag.WRAPPING_KEY_DECRYPTED_DATA_KEY
 }
 
 const edk1 = new EncryptedDataKey({ providerId: 'keyNamespace', providerInfo: 'keyName', encryptedDataKey: new Uint8Array([1]) })
@@ -48,7 +53,7 @@ const cryptoKey: any = { type: 'secret', algorithm: { name: webCryptoSuite.encry
 describe('cloneMaterial', () => {
   it('clone NodeEncryptionMaterial', () => {
     const material = new NodeEncryptionMaterial(nodeSuite, { some: 'context' })
-      .setUnencryptedDataKey(new Uint8Array(udk128), trace)
+      .setUnencryptedDataKey(new Uint8Array(udk128), encryptTrace)
       .addEncryptedDataKey(edk1, KeyringTraceFlag.WRAPPING_KEY_ENCRYPTED_DATA_KEY)
       .addEncryptedDataKey(edk2, KeyringTraceFlag.WRAPPING_KEY_ENCRYPTED_DATA_KEY)
 
@@ -62,7 +67,7 @@ describe('cloneMaterial', () => {
 
   it('clone NodeDecryptionMaterial', () => {
     const material = new NodeDecryptionMaterial(nodeSuite, { some: 'context' })
-      .setUnencryptedDataKey(new Uint8Array(udk128), trace)
+      .setUnencryptedDataKey(new Uint8Array(udk128), decryptTrace)
 
     const test = cloneMaterial(material)
     expect(test).to.be.instanceOf(NodeDecryptionMaterial)
@@ -73,8 +78,8 @@ describe('cloneMaterial', () => {
 
   it('clone WebCryptoEncryptionMaterial', () => {
     const material = new WebCryptoEncryptionMaterial(webCryptoSuite, { some: 'context' })
-      .setUnencryptedDataKey(new Uint8Array(udk128), trace)
-      .setCryptoKey(cryptoKey, trace)
+      .setUnencryptedDataKey(new Uint8Array(udk128), encryptTrace)
+      .setCryptoKey(cryptoKey, encryptTrace)
       .addEncryptedDataKey(edk1, KeyringTraceFlag.WRAPPING_KEY_ENCRYPTED_DATA_KEY)
       .addEncryptedDataKey(edk2, KeyringTraceFlag.WRAPPING_KEY_ENCRYPTED_DATA_KEY)
 
@@ -90,12 +95,43 @@ describe('cloneMaterial', () => {
   it('clone WebCryptoDecryptionMaterial', () => {
     /* WebCryptoDecryptionMaterial do not have an unencrypted data key. */
     const material = new WebCryptoDecryptionMaterial(webCryptoSuite, { some: 'context' })
-      .setCryptoKey(cryptoKey, trace)
+      .setCryptoKey(cryptoKey, decryptTrace)
 
     const test = cloneMaterial(material)
     expect(test).to.be.instanceOf(WebCryptoDecryptionMaterial)
     expect(test.getCryptoKey()).to.deep.equal(cryptoKey)
     expect(test.keyringTrace).to.deep.equal(material.keyringTrace)
     expect(test.encryptionContext).to.deep.equal(material.encryptionContext)
+  })
+
+  it('Precondition: For each encrypted data key, there must be a trace.', () => {
+    const material = new NodeEncryptionMaterial(nodeSuite, { some: 'context' })
+      .setUnencryptedDataKey(new Uint8Array(udk128), encryptTrace)
+      .addEncryptedDataKey(edk1, KeyringTraceFlag.WRAPPING_KEY_ENCRYPTED_DATA_KEY)
+      .addEncryptedDataKey(edk2, KeyringTraceFlag.WRAPPING_KEY_ENCRYPTED_DATA_KEY)
+
+    // remove a trace...
+    material.keyringTrace.pop()
+    expect(() => cloneMaterial(material)).to.throw(Error, 'KeyringTrace length does not match encrypted data keys.')
+  })
+
+  it('Precondition: The traces must be in the same order as the encrypted data keys.', () => {
+    const material = new NodeEncryptionMaterial(nodeSuite, { some: 'context' })
+      .setUnencryptedDataKey(new Uint8Array(udk128), encryptTrace)
+      .addEncryptedDataKey(edk1, KeyringTraceFlag.WRAPPING_KEY_ENCRYPTED_DATA_KEY)
+      .addEncryptedDataKey(edk2, KeyringTraceFlag.WRAPPING_KEY_ENCRYPTED_DATA_KEY)
+
+    // @ts-ignore Typescript is trying to save us...
+    material.keyringTrace[1].keyName = 'does not exist'
+    expect(() => cloneMaterial(material)).to.throw(Error, 'Keyring trace does not match encrypted data key.')
+  })
+
+  it('Precondition: On Decrypt there must not be any additional traces other than the setTrace.', () => {
+    const material = new NodeDecryptionMaterial(nodeSuite, { some: 'context' })
+      .setUnencryptedDataKey(new Uint8Array(udk128), decryptTrace)
+
+    // Just push _something_ on
+    material.keyringTrace.push({} as any)
+    expect(() => cloneMaterial(material)).to.throw(Error, 'Only 1 trace is valid on DecryptionMaterials.')
   })
 })
