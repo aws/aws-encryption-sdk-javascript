28d57e63fefc125783b5fd64338b5116e5fe9aa6
pmaksimo <p.maksimovic@imperial.ac.uk>
2019-09-18 19:12:58 +0100

    fix: prototype inheritance bug in decodeEncryptionContext, additional test vector in fixtures.ts
    If the serialised encryption context contains a key-value pair where the key coincides with a property of Object.prototype, decodeEncryptionContext will incorrectly throw 'Error: Duplicate encryption context key value'.
    
    The fix is to create the encryptionContext using Object.create(null) instead of the standard object initialiser.
    
    This commit also adds a test vector in fixtures.ts, containing ‘hasOwnProperty’ as its only key.
    
    Nota bene: This fix disables the standard calling of Object.prototype functions on encryption contexts returned by decodeEncryptionContext. In particular, it is no longer possible to call encryptionContext.hasOwnProperty(prop). However, due to the fix, one can use the simpler encryptionContext[prop] instead.

--- modules/serialize/test/deserialize_factory.test.ts
+++ modules/serialize/test/deserialize_factory.test.ts
@@ -82,20 +82,8 @@ describe('deserializeFactory:decodeEncryptionContext', () => {
   it('Keys may be properties of Object.prototype, decodeEncryptionContext has to succeed', () => {
     const { decodeEncryptionContext } = deserializeFactory(toUtf8, WebCryptoAlgorithmSuite)
 
-    /* Single key-value pair */
-    const kvPairCount = Buffer.alloc(2)
-    kvPairCount.writeUInt16BE(1, 0)
-
-    /* Actual key-value pair */
-    const kvPair = [ 'hasOwnProperty', 'arbitraryValue' ]
-
-    /* Constructing the encryption context */
-    const kvPairBinary = kvPair.map(str => new Uint8Array([...Buffer.from(str)]))
-    const encryptionContext = concatBuffers(kvPairCount, ...kvPairBinary.map(bufStr => {
-      const len = Buffer.alloc(2)
-      len.writeUInt16BE(bufStr.byteLength, 0)
-      return concatBuffers(len, bufStr)
-    }))
+    /* hasOwnProperty test vector */
+    const encryptionContext = fixtures.hasOwnPropertyEncryptionContext().slice(2)
 
     const test = decodeEncryptionContext(encryptionContext)
     expect(test).to.have.property('hasOwnProperty')
