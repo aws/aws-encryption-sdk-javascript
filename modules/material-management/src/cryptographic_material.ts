f4a6e41c5a3ff34b4f0e3fb1d7af708aea961723
seebees <ryanemer@amazon.com>
2019-10-14 12:45:10 -0700

    Update modules/material-management/src/cryptographic_material.ts
    Co-Authored-By: Matt Bullock <bullocm@amazon.com>

--- modules/material-management/src/cryptographic_material.ts
+++ modules/material-management/src/cryptographic_material.ts
@@ -83,7 +83,7 @@ const timingSafeEqual: (a: Uint8Array, b: Uint8Array) => boolean = (function () 
   function portableTimingSafeEqual (a: Uint8Array, b: Uint8Array) {
     /* It is *possible* that a runtime could optimize this constant time function.
      * Adding `eval` could prevent the optimization, but this is no guarantee.
-     * The eval below is commented out,
+     * The eval below is commented out
      * because if a browser is using a Content Security Policy with `'unsafe-eval'`
      * it would fail on this eval.
      * The value in attempting to ensure this function is not optimized,
