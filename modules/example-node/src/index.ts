b34124487d44ff6c9726e89dd73f8cce12acbd49
seebees <ryanemer@amazon.com>
2020-04-21 10:15:03 -0700

    Update modules/example-node/src/index.ts
    Co-Authored-By: Matt Bullock <bullocm@amazon.com>

--- modules/example-node/src/index.ts
+++ modules/example-node/src/index.ts
@@ -1,9 +1,6 @@
 // Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-// Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: Apache-2.0
-
 /*
  * This library has no exported implementation.
  * It is intended to be used as a reference.
