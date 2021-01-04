4de58666129a3a1ba059591045af56b4e9678d46
seebees <ryanemer@amazon.com>
2021-01-04 13:42:06 -0800

    fix: better typing from typescript

--- modules/decrypt-node/src/decrypt.ts
+++ modules/decrypt-node/src/decrypt.ts
@@ -60,7 +60,7 @@ export async function _decrypt(
 }
 
 async function finishedAsync(stream: Duplex) {
-  return new Promise((resolve, reject) => {
+  return new Promise<void>((resolve, reject) => {
     finished(stream, (err: Error) => (err ? reject(err) : resolve()))
   })
 }
