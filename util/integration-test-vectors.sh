c77bb08ad8d60652962580c9c1a5f41e64506df3
Lucas McDonald <lucasmcdonald3@gmail.com>
2026-07-22 15:58:11 -0700

    fix(release): source test vector values from one file so npm 12 can run scripts (#1684)

---
+++ util/integration-test-vectors.sh
@@ -0,0 +1,9 @@
+# Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+# Single source of truth for integration test vector locations.
+# Sourced by the integration-* and verdaccio-* npm scripts.
+export LOCAL_TEST_VECTORS="aws-encryption-sdk-test-vectors/vectors/awses-decrypt/python-2.3.0.zip"
+export ENCRYPT_MANIFEST_LIST="https://raw.githubusercontent.com/awslabs/aws-crypto-tools-test-vector-framework/master/features/CANONICAL-GENERATED-MANIFESTS/0003-awses-message-encryption.v1.json"
+export ENCRYPT_KEY_MANIFEST="https://raw.githubusercontent.com/awslabs/aws-crypto-tools-test-vector-framework/master/features/CANONICAL-GENERATED-MANIFESTS/0002-keys.v1.json"
+export DECRYPT_ORACLE="https://xi1mwx3ttb.execute-api.us-west-2.amazonaws.com/api/v0/decrypt"
